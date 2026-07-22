07792fd965addffbfdd5e0a746ce81f28a952d5c
Tim <0xtimc@gmail.com>
2026-07-22 10:50:15 +0100

    Add basic Xcode project

---
+++ Mixtape/Mixtape/MixtapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MixtapeApp.swift
+//  Mixtape
+//
+//  Created by Tim Condon on 22/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct MixtapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mixtape/MixtapeTests/MixtapeTests.swift
@@ -0,0 +1,19 @@
+//
+//  MixtapeTests.swift
+//  MixtapeTests
+//
+//  Created by Tim Condon on 22/07/2026.
+//
+
+import Testing
+@testable import Mixtape
+
+struct MixtapeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
